c6ad06caa2a1da5b4b273aa0853b007076c762b1
juliahumeniuk <rosa.lima.julia@escola.pr.gov.br>
2025-06-05 12:26:04 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -22,3 +22,49 @@ criaCartao(
     'Quando occoreu a Queda da Bastilha?',
     'Em 14 de julho de 1789'
 )
+criaCartao(
+    'Sociologia',
+    'Qual é o objeto de estudo de Émile Durkheim?',
+    'São o fatos sociais'
+)
+
+criaCartao(
+    'PortuguêS',
+    'Quem escreveu o romance "Senhora"?',
+    'Foi José de Alencar'
+)
+
+criaCartao(
+    'Matématica',
+    'Qual é o valor e Pi?',
+    'É 3,1415'
+)
+
+criaCartao(
+    'Química',
+    'Quantos elétrons tem na quarta camada da eletrofera?',
+    'Cabem no máximo 32 elétrons'
+)
+
+criaCartao(
+    'Física 1',
+    'O que é um led?',
+    'É um diodo emissor de luz'
+)
+
+criaCartao(
+    'Projeto de vida',
+    'O que é empresa virtual?',
+    'é um negócio que opera principalmente através da internet, utilizando tecnologias digitais para realizar suas atividades comerciais'
+)
+
+criaCartao(
+    'Educação Financeira ',
+    'Quando criaram o dólar?',
+    'Em  2 de abril de 1792 '
+)
+criaCartao(
+    'Inglês',
+    'Qual a tradução e "Fairness"?',
+    'Signifca jutiça, equidade '
+)
