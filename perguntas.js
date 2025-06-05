73c8ddd46741db220b551f54d2ac25d3ca3afaa5
juliahumeniuk <rosa.lima.julia@escola.pr.gov.br>
2025-06-05 10:59:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Biologia',
+    'Quantas filos exitem?',
+    'Exitem 35 filo, embora 9 sejam conhecidas e compreendidas'
+)
+
+criaCartao(
+    'Geografia',
+    'Quem foi o criador dos domínios morfoclimáticos?',
+    'Foi Aziz Nacib Ab Saber'
+)
+
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'História',
+    'Quando occoreu a Queda da Bastilha?',
+    'Em 14 de julho de 1789'
+)
